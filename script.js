6838c634041a0fa038a6a7461b75b09848c0e33e
anshoria <anshoria.forwork@gmail.com>
2024-07-13 15:12:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// efek scrool pada navbar
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.remove('transparent');
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+      navbar.classList.add('transparent');
+    }
+  });
+  
+  // Set initial state
+  document.addEventListener('DOMContentLoaded', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.remove('transparent');
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.add('transparent');
+    }
+  });
+  
+
+//   nav.active
+// script.js
+document.addEventListener('DOMContentLoaded', function() {
+    var navItems = document.querySelectorAll('.nav-item .nav-link');
+  
+    navItems.forEach(function(item) {
+      item.addEventListener('click', function() {
+        // Remove 'active' class from all nav items
+        navItems.forEach(function(nav) {
+          nav.classList.remove('active');
+        });
+  
+        // Add 'active' class to the clicked nav item
+        this.classList.add('active');
+      });
+    });
+  });
+  
